6dcf3453e490b1c7e7702241e6afc078eaca3364
Swann <swann.bouviermuller@gmail.com>
2022-02-22 09:51:10 -0500

    add configuration

--- config/settings.py
+++ config/settings.py
@@ -9,35 +9,56 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
 from pathlib import Path
+import pkg_resources
 
+
+root = environ.Path(__file__) - 2  # get root of the project
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(root())
+
+# initialize env with environment variable
+# it can contains DEBUG (in production env it shall)
+# so we could explore how to load file only on local execution
+env = environ.Env()
+
+env_path = BASE_DIR / ".env"
+if env_path.is_file():
+    environ.Env.read_env(str(env_path))  # reading .env file
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6l__-#_)koi3lke#_a&pgi^^s+s1(pec9!+p+31nrpro0nnkr_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = env.str("SECRET")
+DEBUG = env.bool("DEBUG", default=False)
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "django.contrib.humanize",
 ]
+
+THIRD_APPS = [
+    "crispy_forms",
+]
+
+PROJECT_APPS = [
+    "blog.apps.BlogConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +75,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,10 +97,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(),
 }
 
 
@@ -86,7 +106,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -98,6 +119,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# indicate the new User model to Django
+AUTH_USER_MODEL = "users.User"
+LOGIN_REDIRECT_URL = "carto:home_connected"
+LOGIN_URL = "users:signin"
 
 
 # Internationalization
@@ -115,9 +142,76 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = str(BASE_DIR / "staticroot")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # BASE_DIR / "htmlcov",
+]
+
+# same goes for media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# specify to django we use only S3 to store files
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# credentials
+AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID", default="")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY", default="")
+
+# bucket name and region
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME", default="")
+AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME", default="eu-west-3")
+
+# append a prefix to all uploaded file (useful to not mix local, staging...)
+AWS_LOCATION = env.str("AWS_LOCATION", default="local")
+# avoid overriding a file if same key is provided
+AWS_S3_FILE_OVERWRITE = False
+# allow signed url to be accessed from all regions
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+# Crispy configuration
+# https://django-crispy-forms.readthedocs.io/en/latest/index.html
+# set default rendering
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
+# django-debug-toolbar configuration
+
+# activate only if debug is True
+# and only if package is available
+if DEBUG:
+    try:
+        # only if debug_toolbar is available, else it will fire exception
+        # useful to turn debug mode on staging without installing dev dependencies
+        import debug_toolbar  # noqa: F401
+
+        INSTALLED_APPS += ["debug_toolbar"]
+        MIDDLEWARE += [
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+
+        # bypass check of internal IPs
+        def show_toolbar(request):
+            return True
+
+        DEBUG_TOOLBAR_CONFIG = {
+            "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+        }
+
+    except ImportError:
+        pass
+
+    if "django-extensions" in {pkg.key for pkg in pkg_resources.working_set}:
+        INSTALLED_APPS += [
+            "django_extensions",
+        ]
